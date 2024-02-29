dc0540c61bdafc059ee3441137d22a484dc98469
Aditya Parmar <19276544@brookes.ac.uk>
2024-02-29 12:45:27 +0000

    feat(group-22): added the Brookes logo to the login page, added button submit text and design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,10 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      'brookes':'#d10373',
+      'bigwhite':'#ffffff'
+    }
   },
   plugins: [require('@tailwindcss/forms'),],
 };
