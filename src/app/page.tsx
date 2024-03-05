57ffd2477a954bf23bc0e98fdaeaf756ae15b664
Genesis-bianca1 <19141230@brookes.ac.uk>
2024-03-05 11:04:53 +0000

    feat(group_29): route.ts code updated and page.tsx boilerplate updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,85 +1,55 @@
 // Parent component
 'use client';
 
-import { useState, FormEvent } from 'react';
+import { useState, FormEvent, useEffect } from 'react';
+import { I_post } from '../app/api/posts/route';
+import { UserIcon } from '@heroicons/react/20/solid';
 
-const posts = [
-  {
-    id: 1,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
-    author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
+const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
 
-  {
-    id: 2,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
-    author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  {
-    id: 3,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
-    author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  {
-    id: 4,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
-    author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  // More posts...
-];
+  // Format as yyyy-mm-dd
+  const formattedDate = date.toISOString().split('T')[0];
 
-export default function SearchPostsByLabels() {
+  // Format as Month dd, yyyy
+  const formattedDateLong = new Intl.DateTimeFormat('en-US', {
+    month: 'short',
+    day: '2-digit',
+    year: 'numeric',
+  }).format(date);
+
+  return [formattedDate, formattedDateLong];
+};
+
+export default function Home() {
   const [searchInput, setSearchInput] = useState('');
+  const [posts, setPosts] = useState<(I_post & { date: string; datetime: string })[]>([]);
 
-  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`/api/posts`);
+      if (!response.ok) {
+        throw new Error('Failed to search posts by labels');
+      }
+      const data = await response.json();
+
+      setPosts(
+        data.results.map((post: I_post) => {
+          const [shortFormat, longFormat] = formatDate('2024-02-24T21:35:23.106Z');
+
+          return {
+            ...post,
+            date: longFormat,
+            datetime: shortFormat,
+          };
+        }),
+      );
+    }
+
+    fetchData();
+  }, []);
+
+  async function handleSearch(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     // Parse URL parameters to extract labels
@@ -97,8 +67,18 @@ export default function SearchPostsByLabels() {
         throw new Error('Failed to search posts by labels');
       }
       const data = await response.json();
-      console.log('Search results:', data.posts);
-      // Handle search results
+
+      setPosts(
+        data.results.map((post: I_post) => {
+          const [shortFormat, longFormat] = formatDate('2024-02-24T21:35:23.106Z');
+
+          return {
+            ...post,
+            date: longFormat,
+            datetime: shortFormat,
+          };
+        }),
+      );
     } catch (error) {
       console.error('Error searching posts by labels:', error);
       // Handle error
@@ -106,71 +86,76 @@ export default function SearchPostsByLabels() {
   }
 
   return (
-    <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
-          <p className="mt-2 text-lg leading-8 text-gray-600">
-            Learn how to grow your business with our expert advice.
-          </p>
-        </div>
-
-        <form onSubmit={handleSubmit} className="mt-6">
-          <div className="flex items-center">
-            <input
-              type="text"
-              placeholder="Enter search terms separated by commas"
-              value={searchInput}
-              onChange={(e) => setSearchInput(e.target.value)}
-              className="border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 block w-1/3 rounded-md sm:text-sm border p-2"
-            />
-            <button
-              type="submit"
-              className="ml-2 inline-flex justify-center w-24 rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base leading-6 font-medium text-white hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 sm:text-sm"
-            >
-              Search
-            </button>
+    <main>
+      <img src="/logo_2.png" alt="logo" className="absolute top-5 left-12 h-25 w-60 p-0" />
+      <div className="bg-gray-200 py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8 relative">
+          <div className="bg-yellow-500 py-8 px-5 top-0 left-0 right-0 ml-0">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl">TDE Research Blog</h2>
+            <p className="mt-2 text-lg leading-8 text-gray-600">
+              Research in the Faculty of TDE is based around powerful well led research groupings that benefit from the
+              high level complementary skills and expertise of their members.
+            </p>
           </div>
-        </form>
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {posts.map((post) => (
-            <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
-              <div className="flex items-center gap-x-4 text-xs">
-                <time dateTime={post.datetime} className="text-gray-500">
-                  {post.date}
-                </time>
-                <a
-                  href={post.category.href}
-                  className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                >
-                  {post.category.title}
-                </a>
-              </div>
-              <div className="group relative">
-                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                  <a href={post.href}>
-                    <span className="absolute inset-0" />
-                    {post.title}
-                  </a>
-                </h3>
-                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
-              </div>
-              <div className="relative mt-8 flex items-center gap-x-4">
-                <img src={post.author.imageUrl} alt="" className="h-10 w-10 rounded-full bg-gray-50" />
-                <div className="text-sm leading-6">
-                  <p className="font-semibold text-gray-900">
-                    <a href={post.author.href}>
-                      <span className="absolute inset-0" />
-                      {post.author.name}
-                    </a>
-                  </p>
-                  <p className="text-gray-600">{post.author.role}</p>
+
+          <form onSubmit={handleSearch} className="mt-6">
+            <div className="flex items-center">
+              <input
+                type="text"
+                placeholder="Enter search terms separated by commas"
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
+                className="border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 block w-1/3 rounded-md sm:text-sm border p-2"
+              />
+              <button
+                type="submit"
+                className="ml-2 inline-flex justify-center w-24 rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base leading-6 font-medium text-white hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 sm:text-sm"
+              >
+                Search
+              </button>
+            </div>
+          </form>
+          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            {posts.map((post) => (
+              <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
+                <div className="flex items-center gap-x-4 text-xs">
+                  <time dateTime={post.datetime} className="text-gray-500">
+                    {post.date}
+                  </time>
+                  {post.labels.map((postLabel) => (
+                    <span
+                      key={`${postLabel.postId}_${postLabel.labelId}`}
+                      className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10"
+                    >
+                      {postLabel.label.name}
+                    </span>
+                  ))}
                 </div>
-              </div>
-            </article>
-          ))}
+                <div className="group relative">
+                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                    <a>
+                      <span className="absolute inset-0" />
+                      {post.title}
+                    </a>
+                  </h3>
+                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.content}</p>
+                </div>
+                <div className="relative mt-8 flex items-center gap-x-4">
+                  <UserIcon className="h-10 w-10 rounded-full bg-gray-50" />
+                  <div className="text-sm leading-6">
+                    <p className="font-semibold text-gray-900">
+                      <a>
+                        <span className="absolute inset-0" />
+                        {post.author.firstName} {post.author.lastName}
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </article>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
