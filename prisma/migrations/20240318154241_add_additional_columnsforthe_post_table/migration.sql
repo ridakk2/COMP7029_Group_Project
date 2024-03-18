3817eb61bee8bcb405f5aad15f76d5132887096e
Aditya Parmar <19276544@brookes.ac.uk>
2024-03-18 16:08:41 +0000

    fix(group-63): to add additional lables in the post creation

---
+++ prisma/migrations/20240318154241_add_additional_columnsforthe_post_table/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "abstract" VARCHAR(255),
+ADD COLUMN     "methodology" VARCHAR(255),
+ADD COLUMN     "potentialAppl" VARCHAR(255),
+ADD COLUMN     "projectGoals" VARCHAR(255);
